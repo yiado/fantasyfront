f2850e2de2f8355fae0a5e14a0ca581c95b1710a
YmendezM <ysmendezm@gmail.com>
2021-09-15 14:12:53 -0300

    Comentar uuser pruebas

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -20,13 +20,13 @@ export const ROUTES: RouteInfo[] = [
     rtlTitle: "قائمة الجدول",
     icon: "icon-chart-bar-32",
     class: ""
-  },{
+  }/*,{
     path: "/user",
     title: "User Profile",
     rtlTitle: "ملف تعريفي للمستخدم",
     icon: "icon-single-02",
     class: ""
-  }/*,
+  },
   {
     path: "/icons",
     title: "Icons",
