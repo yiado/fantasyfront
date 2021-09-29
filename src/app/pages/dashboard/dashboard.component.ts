5cf44bfbf0ea8211ec5964bb67e4ffd875357fd3
YmendezM <ysmendezm@gmail.com>
2021-09-29 01:21:50 -0300

    Conteos de torneos

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,7 +2,9 @@ import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
 import { Component, OnInit } from "@angular/core";
 import Chart from 'chart.js';
 import { Jugador } from "src/app/interface/jugador";
+import { Ranking } from "src/app/interface/ranking";
 import { JugadorService } from 'src/app/service/jugador.service';
+import { RankingService } from 'src/app/service/ranking.service';
 
 
 @Component({
@@ -21,6 +23,7 @@ export class DashboardComponent implements OnInit {
   public clicked1: boolean = false;
   public clicked2: boolean = false;
   public Rankings : Jugador[];
+  public RankingsGeneral : Ranking[];
   public NombreTabla : String;  
   public Posicion : String; 
   public Nombre : String; 
@@ -32,7 +35,7 @@ export class DashboardComponent implements OnInit {
 
 
 
-  constructor(private jugadorService: JugadorService) {
+  constructor(private jugadorService: JugadorService, private rankingService: RankingService) {
 
     this.NombreTabla = "Tabla General";
     this.Posicion = "Nº";
@@ -51,13 +54,37 @@ export class DashboardComponent implements OnInit {
     this.jugadorService.getAllJugador()
     .subscribe(jugador =>{
       this.Rankings = jugador
-      //console.log(ranking)
+      //console.log(jugador)
     });
+  }
+
+  getAllRankingGeneral(){
+    this.rankingService.getAllRankingGeneral()
+    .subscribe(jugador =>{
+      this.RankingsGeneral = jugador
+      //console.log(this.RankingsGeneral)
+      var myObj = []
+      for (const jugador in this.RankingsGeneral) {
+        let jug = this.RankingsGeneral[jugador]
+        //console.log(jugador);
+        //console.log(jug['nombre']);
+        
+        if(!myObj.some(myObj => myObj.nombre === jug['jugadorId']['nombre'])){
+          myObj.push({id: jug['jugadorId']['id'], nombre: jug['jugadorId']['nombre'], pais: jug['jugadorId']['pais'], residencia: jug['jugadorId']['residencia'], puntos: jug['puntos']})
+        }
+    
+      }
+      console.log(myObj)
+      //console.log(this.RankingsGeneral)
+      
+    });
+
   }
 
   ngOnInit() {
   
-    this.getAllJugador()
+    this.getAllJugador();
+    this.getAllRankingGeneral()
 
   }
   public updateOptions() {
--- src/app/service/ranking.service.ts
+++ src/app/service/ranking.service.ts
@@ -19,4 +19,14 @@ export class RankingService {
     }
     return this.http.get<Ranking[]>(path, {headers});
   }
+
+  getAllRankingGeneral(){
+
+    const path = 'http://ec2-52-43-130-134.us-west-2.compute.amazonaws.com:2003/v1/inscripciones/true';
+    const headers = { 
+      'Content-Type': 'application/json', 
+      //'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBZG1pbiIsImV4cCI6MTYyNzc1OTI0NX0.YbT6ZK-nrX9RnESlDM_bM3zUcnFNmWh_qWyE8IOL_AQ5osUAXYIe2k15JVs4RbOw1bdt-9iz6WHqxjE1xedi3w'
+    }
+    return this.http.get<Ranking[]>(path, {headers});
+  }
 }
